277c12d435b0bb34e4f14a574fae3914aa9a2873
Iylo <theagle43@gmail.com>
2024-02-13 20:12:03 -0700

    v1.0 commit

--- src/main/java/com/iylo/create_wooderwheels/Create_WooderWheels.java
+++ src/main/java/com/iylo/create_wooderwheels/Create_WooderWheels.java
@@ -1,9 +1,13 @@
 package com.iylo.create_wooderwheels;
 
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("create_wooderwheels")
 public class Create_WooderWheels {
+
+    public static final Logger logger = LogManager.getLogger("create_wooderwheels");
     
 }
--- src/main/java/com/iylo/create_wooderwheels/mixins/WaterWheelRendererMixin.java
+++ src/main/java/com/iylo/create_wooderwheels/mixins/WaterWheelRendererMixin.java
@@ -6,68 +6,128 @@ import com.simibubi.create.content.kinetics.waterwheel.WaterWheelBlockEntity;
 import com.simibubi.create.content.kinetics.waterwheel.WaterWheelRenderer;
 import com.simibubi.create.foundation.model.BakedModelHelper;
 import com.simibubi.create.foundation.utility.RegisteredObjects;
+import it.unimi.dsi.fastutil.objects.Reference2ReferenceOpenHashMap;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.spongepowered.asm.mixin.*;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.stream.Stream;
+import java.util.Map;
+
+import static com.iylo.create_wooderwheels.Create_WooderWheels.logger;
 
 @Mixin(value = WaterWheelRenderer.class, remap = false)
 public abstract class WaterWheelRendererMixin<T extends WaterWheelBlockEntity> extends KineticBlockEntityRenderer<T> {
 
+    @Shadow
+    @Final
+    public static StitchedSprite OAK_PLANKS_TEMPLATE;
 
-    @Shadow @Final public static StitchedSprite OAK_PLANKS_TEMPLATE;
+    @Shadow
+    private static TextureAtlasSprite getSpriteOnSide(BlockState state, Direction side) {
+        return null;
+    }
+
+    @Shadow
+    @Final
+    public static StitchedSprite OAK_LOG_TEMPLATE;
+
+    @Shadow
+    @Final
+    public static StitchedSprite OAK_LOG_TOP_TEMPLATE;
+
+    @Shadow
+    @Final
+    private static String[] LOG_SUFFIXES;
 
     public WaterWheelRendererMixin(BlockEntityRendererProvider.Context context) {
         super(context);
     }
 
-    @Inject(method = "generateModel*", at = @At("TAIL"))
-    public BakedModel ongenerateModel(BakedModel template, BlockState planksBlockState){
+    /**
+     * @author iylo
+     * @reason Compatibility with non-Vanilla wood types.
+     */
+    @Overwrite
+    public static BakedModel generateModel(BakedModel template, BlockState planksBlockState) {
         Block planksBlock = planksBlockState.getBlock();
         ResourceLocation id = RegisteredObjects.getKeyOrThrow(planksBlock);
-        String path = id.getPath();
-        Item planksItem = planksBlock.asItem();
-        ItemStack planksStack = new ItemStack(planksItem);
+        ItemStack planksStack = new ItemStack(planksBlock.asItem());
         TagKey<Item> planksTag = ItemTags.PLANKS;
-        Stream<TagKey<Item>> planksStackTags = planksStack.getTags();
-        TagKey<Item> logsTag = ItemTags.LOGS;
+        String namespace = id.getNamespace();
+        logger.info("Plank namespace is " + namespace);
 
-        if(planksStack.is(planksTag)){
-            String name = String.valueOf(planksStack.getDisplayName()).strip().toLowerCase();
-            String[] nameArray = name.split(" ");
-            Iterator<String> itNameArray = Arrays.stream(nameArray).iterator();
-            StringBuilder plankNameString = new StringBuilder();
+        if (planksStack.is(planksTag)) {
+            String planksName = planksStack.getDisplayName().getString().strip().toLowerCase();
+            String wood = planksName.replace(" planks", "")
+                    .replace(" ", "_")
+                    .replace("[", "")
+                    .replace("]", "");
 
-            while(itNameArray.hasNext()){
-                String next = itNameArray.next();
-                    if(next.equals("planks")){
-                        itNameArray.remove();
-                    } else {
-                        plankNameString.append(next);
-                        itNameArray.remove();
-                        if(itNameArray.hasNext()){
-                            plankNameString.append("_");
-                        }
-                    }
-            }
+            BlockState logBlockState = getLogBlockState(namespace, wood);
+
+            Map<TextureAtlasSprite, TextureAtlasSprite> map = new Reference2ReferenceOpenHashMap<>();
+            map.put(OAK_PLANKS_TEMPLATE.get(), getSpriteOnSide(planksBlockState, Direction.UP));
+            map.put(OAK_LOG_TEMPLATE.get(), getSpriteOnSide(logBlockState, Direction.SOUTH));
+            map.put(OAK_LOG_TOP_TEMPLATE.get(), getSpriteOnSide(logBlockState, Direction.UP));
+
+            return BakedModelHelper.generateModel(template, map::get);
         }
 
         return BakedModelHelper.generateModel(template, sprite -> null);
     }
 
+    /**
+     * @author iylo
+     * @reason Compatibility with non-Vanilla woods
+     */
 
+
+    @Overwrite
+    private static BlockState getLogBlockState(String namespace, String wood) {
+        Block logBlock;
+        TagKey<Block> logsTag = BlockTags.LOGS;
+        Object[] blockList = ForgeRegistries.BLOCKS.tags().getTag(logsTag).stream().toArray();
+        //DEBUG: logger.info("There are " + blockList.length + " Blocks in the blockList!");
+        for (Object object : blockList) {
+            Block block = (Block) object;
+            ItemStack logStack = new ItemStack(block.asItem());
+            String logName = logStack.getDisplayName().getString().strip().toLowerCase();
+            //DEBUG: logger.info("logName is " + logName);
+            for (String suffix : LOG_SUFFIXES) {
+                //DEBUG: logger.info("suffix is " + suffix);
+                try {
+                    String logWood = logName
+                            .replace(" ", "_")
+                            .replace("[", "")
+                            .replace("]", "");
+                    logWood = logWood.replace(suffix, "");
+                    //DEBUG: logger.info("logWood is " + logWood);
+                    //DEBUG: logger.info("wood is " + wood);
+                    if (logWood.equals(wood)) {
+                        logBlock = block;
+                        ResourceLocation id = RegisteredObjects.getKeyOrThrow(logBlock);
+                        logger.info("Log namespace is " + id.getNamespace());
+                        String logNamespace = id.getNamespace();
+                        if(logNamespace.equals(namespace)) {
+                            return logBlock.defaultBlockState();
+                        }
+                    }
+                } catch (Exception ignored) {
+                }
+            }
+        }
+        return Blocks.OAK_LOG.defaultBlockState();
+    }
 }
